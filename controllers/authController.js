474c50dd0df6fee08c77bda88299a460a1789ddb
andri2621 <andri.setiawan996@gmail.com>
2021-03-10 03:19:16 +0700

    edit token login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,24 +3,24 @@ const jwt    = require('jsonwebtoken')
 
 const login = (req,res,next) => {
     // const userName = req.body.userName,
-    const emailAddress = req.body.emailAddress
+    // const emailAddress = req.body.emailAddress
 
-    User.findOne({emailAddress})
-    .then(user => {
-        if(user) {
+    // User.findOne({emailAddress})
+    // .then(user => {
+    //     if(user) {
             const token = jwt.sign({
-                emailAddress: emailAddress
+                name: 'Andi Setiawan'
                 }, 'secret')
             res.json({
                 message: 'Login Berhasil',
                 token
             })
-        }else {
-            res.json({
-                message: 'Email tidak sama'
-            })
-        }
-    })
+    //     }else {
+    //         res.json({
+    //             message: 'Email tidak sama'
+    //         })
+    //     }
+    // })
 }
 
 module.exports = {
